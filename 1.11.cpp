7cfec9833ee8d3b2111e31487d5de4065a4ef404
Reon0429-cat <oonishireon110@gmail.com>
2021-12-12 21:46:30 +0900

    1.11.cpp

---
+++ 1.11.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  for (int i = 0; i<5; i++) {
+    if (i == 3) {
+      cout << "REON" << i << endl;
+      break;
+    }
+    continue;
+    cout << i << endl;
+  }
+
+}
